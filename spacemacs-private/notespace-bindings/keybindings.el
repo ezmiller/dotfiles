525889b8220db1e101d91248d1c3efaa04569a4c
Ethan Miller <ethanzanemiller@gmail.com>
2021-01-19 22:37:11 -0800

    Adjust indentation

--- spacemacs-private/notespace-bindings/keybindings.el
+++ spacemacs-private/notespace-bindings/keybindings.el
@@ -1,14 +1,14 @@
 ;; suggested emacs key binding (thanks @mchampine)
 (add-hook 'clojure-mode-hook
-          (lambda ()
-            (define-key clojur
-            (define-key clojure-mode-map (kbd "C-c n r") 'notespace/eval-and-realize-this-notespace)
-            (define-key clojure-mode-map (kbd "C-c n n") 'notespace/eval-and-realize-note-at-this-line)
-            (define-key clojure-mode-map (kbd "C-c n f") 'notespace/eval-and-realize-notes-from-this-line)
-            (define-key clojure-mode-map (kbd "C-c n i b") 'notespace/init-with-browser)
-            (define-key clojure-mode-map (kbd "C-c n i i") 'notespace/init)
-            (define-key clojure-mode-map (kbd "C-c n s") 'notespace/render-static-html)
-            (define-key clojure-mode-map (kbd "C-c n c") 'notespace/eval-and-realize-notes-from-change)))
+  (lambda ()
+    (define-key clojure-mode-map (kbd "C-c n e") 'notespace/eval-this-notespace)
+    (define-key clojure-mode-map (kbd "C-c n r") 'notespace/eval-and-realize-this-notespace)
+    (define-key clojure-mode-map (kbd "C-c n n") 'notespace/eval-and-realize-note-at-this-line)
+    (define-key clojure-mode-map (kbd "C-c n f") 'notespace/eval-and-realize-notes-from-this-line)
+    (define-key clojure-mode-map (kbd "C-c n i b") 'notespace/init-with-browser)
+    (define-key clojure-mode-map (kbd "C-c n i i") 'notespace/init)
+    (define-key clojure-mode-map (kbd "C-c n s") 'notespace/render-static-html)
+    (define-key clojure-mode-map (kbd "C-c n c") 'notespace/eval-and-realize-notes-from-change))))
 
 ;; suggested spacemacs key bindings:
 (spacemacs/set-leader-keys-for-major-mode 'clojure-mode
