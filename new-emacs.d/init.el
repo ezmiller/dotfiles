674d17e29873092d4943a5b27eae8ac6f82dc660
Ethan Miller <ethanzanemiller@gmail.com>
2021-04-25 23:41:58 -0700

    Save start on custom init.el

---
+++ new-emacs.d/init.el
@@ -0,0 +1,115 @@
+(setq delete-old-versions -1)
+(setq inhibit-startup-screen t)
+(setq ring-bell-function 'ignore)
+(setq coding-system-for-read 'utf-8)
+(setq coding-system-for-write 'utf-8)
+(setq sentence-end-double-space nil)
+(setq default-fill-column 80)
+(setq initial-scratch-message "")
+
+(setq-default mode-line-format nil)
+
+(global-auto-revert-mode t)
+(electric-pair-mode 1)
+
+;; Package management
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("org"       . "http://orgmode.org/elpa/")
+			 ("gnu"       . "http://elpa.gnu.org/packages/")
+			 ("melpa"     . "https://melpa.org/packages/")))
+(package-initialize)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+;; Make it easier to restart
+(use-package restart-emacs
+  :defer t)
+
+;; Path management
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize)))
+
+;; Vim mode
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1)
+  (setq-default evil-escape-delay 0.2))
+
+;; Minimal UI
+(scroll-bar-mode -2)
+(tool-bar-mode   -1)
+(tooltip-mode    -1)
+(menu-bar-mode   -1)
+
+;; Themes
+(use-package doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-molokai t))
+
+;; Which Key
+(use-package which-key
+  :ensure t
+  :init
+  (setq which-key-separator " ")
+  (setq which-key-prefix-prefix "+")
+  :config
+  (which-key-mode))
+
+;; Code commenting
+(use-package evil-nerd-commenter :ensure t)
+
+;; Package manager
+(use-package projectile
+  :diminish projectile-mode
+  :config
+  (progn
+    (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+    (projectile-mode +1)
+    (setq projectile-completion-system 'auto)
+    (setq projectile-enable-caching t)
+    (setq projectile-indexing-method 'alien)
+    (add-to-list 'projectile-globally-ignored-files "node-modules")))
+
+;; Selectrum +
+(use-package selectrum
+  :ensure t
+  :config
+  (selectrum-mode +1))
+;; TODO Think about enabling selectrum-prescient
+;; TODO Think about enabling consult: https://github.com/minad/consult
+
+;; Functions
+(defun toggle-buffers ()
+  (interactive)
+  (switch-to-buffer nil))
+
+;; Keybindings
+(use-package general
+  :ensure t
+  :config
+  (general-define-key
+    :states '(normal visual insert emacs)
+    :prefix "SPC"
+    :non-normal-prefix "C-SPC"
+    "TAB" 'toggle-buffers))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(writeroom-mode use-package exec-path-from-shell evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
