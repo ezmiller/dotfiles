884b4b60359ef17f0c653ba095e22a7486df55eb
Ethan Miller <ethanzanemiller@gmail.com>
2021-05-08 12:21:32 -0700

    Update package-slected-packages

--- new-emacs.d/init.el
+++ new-emacs.d/init.el
@@ -288,7 +288,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(dashboard writeroom-mode which-key use-package selectrum-prescient restart-emacs projectile magit general exec-path-from-shell evil-nerd-commenter evil-lisp-state evil-collection doom-themes consult cider)))
+   '(ace-window dashboard writeroom-mode which-key use-package selectrum-prescient restart-emacs projectile magit general exec-path-from-shell evil-nerd-commenter evil-lisp-state evil-collection doom-themes consult cider)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
