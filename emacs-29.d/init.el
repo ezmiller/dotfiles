9b221eb33ae82d7157c65a792668093b5fc573fb
Ethan Miller <ethanzanemiller@gmail.com>
2024-10-30 22:09:18 -0400

    Further refine org journal & org-roam
    -- Establish techwork as default notes lib
    - Set org-journal dir to be relative to org-roam-directory

--- emacs-29.d/init.el
+++ emacs-29.d/init.el
@@ -28,9 +28,9 @@
 ;; line numbers
 (global-display-line-numbers-mode t)
 (dolist (mode '(org-mode-hook
-		term-mode-hook
-		eshell-mode-hook
-		treemacs-mode-hook))
+		  term-mode-hook
+		  eshell-mode-hook
+		  treemacs-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 (use-package autorevert
@@ -510,7 +510,7 @@ same directory as the org-buffer and insert a link to this file."
       (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
       (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
       (clojure "https://github.com/sogaiu/tree-sitter-clojure" "master" "src")
-    (yaml "https://github.com/ikatyang/tree-sitter-yaml" "master" "src")
+	  (yaml "https://github.com/ikatyang/tree-sitter-yaml" "master" "src")
       (json "https://github.com/tree-sitter/tree-sitter-json" "master" "src")))
   (setq major-mode-remap-alist
     '((js2-mode . js-ts-mode)
@@ -612,9 +612,9 @@ same directory as the org-buffer and insert a link to this file."
   :mode ("\\.jsx?\\'" "\\.tsx?\\'" "\\.m?js\\'")
   :hook (((js2-mode
            rjsx-mode
-  	 js-ts-mode
-  	 typescript-ts-mode
-  	 tsx-ts-mode
+	   js-ts-mode
+	   typescript-ts-mode
+	   tsx-ts-mode
            ) . lsp-deferred)) ;; enable lsp-mode
   :config
   (setq lsp-auto-guess-root t)
@@ -731,32 +731,32 @@ same directory as the org-buffer and insert a link to this file."
   :hook (org-mode . my/org-mode-setup)
   :config
   (dolist (face '((org-document-title . 1.4)
-    		(org-level-1 . 1.3)
-    		(org-level-2 . 1.2)
-    		(org-level-3 . 1.1)
-    		(org-level-4 . 1.1)
-    		(org-level-5 . 1.2)
-    		(org-level-6 . 1.2)
-    		(org-level-7 . 1.2)
-    		(org-level-8 . 1.2)))
+  		  (org-level-1 . 1.3)
+  		  (org-level-2 . 1.2)
+  		  (org-level-3 . 1.1)
+  		  (org-level-4 . 1.1)
+  		  (org-level-5 . 1.2)
+  		  (org-level-6 . 1.2)
+  		  (org-level-7 . 1.2)
+  		  (org-level-8 . 1.2)))
     (set-face-attribute (car face) nil
-  		       :font "Roboto Slab"
-  		       :weight 'normal
-  		       :height (cdr face)
-  		       ))
+			 :font "Roboto Slab"
+			 :weight 'normal
+			 :height (cdr face)
+			 ))
 
   ;; replace ellipsis for closed entries
   (set-display-table-slot standard-display-table
-    		      'selective-display (string-to-vector "..."))
+  			'selective-display (string-to-vector "..."))
 
   (setq org-ellipsis " ▾"
-      org-hide-emphasis-markers t ;; hides the special markup symbols arond text
-      org-startup-indented t
-      org-startup-folded 'overview ;; will fold most items
-      org-src-fontify-natively t
+  	org-hide-emphasis-markers t ;; hides the special markup symbols arond text
+  	org-startup-indented t
+  	org-startup-folded 'overview ;; will fold most items
+  	org-src-fontify-natively t
           org-edit-src-content-indentation 2
-      org-fontify-quote-and-verse-blocks t
-      org-fontify-whole-heading-line t))
+  	org-fontify-quote-and-verse-blocks t
+  	org-fontify-whole-heading-line t))
 
 ;; helps with org-mode tables that get messed up sometimes
 (use-package mixed-pitch
@@ -772,8 +772,8 @@ same directory as the org-buffer and insert a link to this file."
   :config
   (setq org-superstar-remove-leading-stars t
         org-superstar-headline-bullets-list '("◉" "○" "●" "○" "●" "○" "●")
-      ;; org-superstar-headline-bullets-list '(" ")
-      ))
+	;; org-superstar-headline-bullets-list '(" ")
+	))
 
 ;; Setup status tags
 (setq org-todo-keywords
@@ -781,11 +781,11 @@ same directory as the org-buffer and insert a link to this file."
 
 (setq org-todo-keyword-faces
       '(("TODO" . (:foreground "#ff39a3" :weight bold))
-      ("STARTED" . "#E35DBF")
-      ("REVIEW" . "lightblue")
-      ("BLOCKED" . "pink")
-      ("CANCELED" . (:foreground "white" :background "#4d4d4d" :weight bold))
-      ("DONE" . "#008080")))
+	("STARTED" . "#E35DBF")
+	("REVIEW" . "lightblue")
+	("BLOCKED" . "pink")
+	("CANCELED" . (:foreground "white" :background "#4d4d4d" :weight bold))
+	("DONE" . "#008080")))
 
 (require 'org-tempo)
 (with-eval-after-load 'org-tempo
@@ -793,8 +793,10 @@ same directory as the org-buffer and insert a link to this file."
   (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp")))
 
 (use-package org-journal
+  :after (org-roam)
   :config
-  (setq org-journal-dir "~/org/techwork/journals/")
+  ;; set org-journal-dir by concatting org-roam-directory with "journals"
+  (setq org-journal-dir (concat org-roam-directory "journals/"))
   (setq org-journal-file-type 'daily)
   (setq org-journal-file-format "%Y-%m-%d.org")
   (setq org-journal-time-prefix "* ")
@@ -807,10 +809,10 @@ same directory as the org-buffer and insert a link to this file."
     (when (= (buffer-size) 0)
       (insert
        (pcase org-journal-file-type
-       (`daily (concat (format-time-string "#+TITLE: %Y-%m-%d") "\n\n"))
-       (`weekly (concat"#+TITLE: Weekly Journal " (format-time-string "(Wk #%V)" time) "\n\n"))
-       (`monthly "#+TITLE: Monthly Journal\n\n")
-       (`yearly "#+TITLE: Yearly Journal\n\n"))))
+	 (`daily (concat (format-time-string "#+TITLE: %Y-%m-%d") "\n\n"))
+	 (`weekly (concat"#+TITLE: Weekly Journal " (format-time-string "(Wk #%V)" time) "\n\n"))
+	 (`monthly "#+TITLE: Monthly Journal\n\n")
+	 (`yearly "#+TITLE: Yearly Journal\n\n"))))
     (concat org-journal-date-prefix (format-time-string "%x" time)))
   (setq org-journal-date-format 'org-journal-date-format-func)
   (setq org-agenda-file-regexp "\\`\\([^.].*\\.org\\|[0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}\\.org\\(\\.gpg\\)?\\)\\'")
@@ -834,6 +836,10 @@ same directory as the org-buffer and insert a link to this file."
    ("C-c n c" . org-roam-capture)
    ("C-c n t" . org-roam-dailies-find-today))
   :init
+  ;; my setup now is that i'm using multiple notes folders
+  ;; the org-roam-directory and other org-roam settings are
+  ;; set locally by .dir-locals.el files. But we'll start here
+  ;; with a "default" on load setting for org-roam-directory.
   (setq org-roam-directory "~/org/techwork/")
   (setq org-roam-dailies-directory "journals/")
   :config
